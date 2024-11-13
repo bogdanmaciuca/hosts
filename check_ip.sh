0278912561c5865d860216ea17210dfda01a16a4
bogdanmaciuca <bogdanmaciuca0621@gmail.com>
2024-11-13 15:15:02 +0200

    added script

---
+++ check_ip.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+filename='/etc/hosts'
+while read line
+do
+    [ "$line" == "" ] && break # Stop on first empty line
+    words=($line)
+    etc_addr=${words[0]}
+    domain=${words[1]}
+     # nslookup afiseaza si ipv6 cateodata
+     # cum ar trebui sa parsez output-ul asa?
+    addr=$(dig +short @8.8.8.8 $domain | head -n 1)
+    [ "$etc_addr" != "127.0.0.1" -a "$etc_addr" != "127.0.1.1" -a "$etc_addr" != "$addr" ] && echo "Bogus IP: $etc_addr"
+done < "$filename"

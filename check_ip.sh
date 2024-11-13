f36733e08e808c0dc440ea96cdb1b14371883408
blindseer1 <vladtiru05@gmail.com>
2024-11-13 15:47:22 +0200

    hello

--- check_ip.sh
+++ check_ip.sh
@@ -17,3 +17,5 @@ do
     addr=$(dig +short @8.8.8.8 $domain | head -n 1)
     [ "$etc_addr" != "127.0.0.1" -a "$etc_addr" != "127.0.1.1" -a "$etc_addr" != "$addr" ] && echo "Bogus IP: $etc_addr"
 done < "$filename"
+
+#Hello 
